d67f9ba0d339bb65cba6db6a45d21b0b2e2e3235
Robin Karlsson <robink90@gmail.com>
2022-10-14 07:59:42 +0200

    lagt till room i MessageModel.cs som var missat

--- Models/MessageModel.cs
+++ Models/MessageModel.cs
@@ -14,16 +14,18 @@ namespace Meddelandecentralen.Models
           Chat
         }
         public Messagetype MessagetypeSetting { get; set; }
+        public string Room { get; set; }
         public string Author { get; set; }
         public string Message { get; set; }
         public string Status { get; set; }
         public string Assigned { get; set; }
 
-        public MessageModel(Guid id, DateTime date, Messagetype messagetypesetting, string author,string message, string status, string assigned)
+        public MessageModel(Guid id, DateTime date, Messagetype messagetypesetting, string room, string author,string message, string status, string assigned)
         {
           Id = id;
           Date = date;
           MessagetypeSetting = messagetypesetting;
+          Room = room;
           Author = author;
           Message = message;
           Status = status;
